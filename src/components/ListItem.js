a51d7d979de5fdb178918d00c2bdbe4585208ad1
Tan_k0 <19522175@gm.uit.edu.vn>
2022-06-16 16:59:10 +0700

    Tuan

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,12 +1,75 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { Audio } from 'expo-av';
 
-const ListItem = ({item}) => {
-    function handleSongPressed(){
-        
+const ListItem = ({item, route }) => {
+
+    const [selectedMusic, setSelectedMusic] = useState (null);
+
+    useEffect(()=>{
+        // let {selectedMusic} = route.params;
+        setSelectedMusic(selectedMusic);
+    },[])
+
+    // const [sound, setSound] = React.useState ();
+    
+    //
+    // const currentTrack = Favorites[selectedMusic];
+    //
+    const [isPlaying, setIsPlaying] = useState (false);
+    const [playbackObject, setPlaybackObject] = useState (null);
+    const [playbackStatus, setPlaybackStatus] = useState (null);
+
+  useEffect(() => {
+    if (playbackObject === null) {
+      setPlaybackObject(new Audio.Sound());
     }
+  }, []);
+
+
+  const handleAudioPlayPause = async () => {
+    if (playbackObject !== null && playbackStatus === null) {
+      const status = await playbackObject.loadAsync(
+
+        // Cach 1 play nhac co san        
+        // selectedMusic.URL,
+        // selectedMusic._id,
+        
+        // { shouldPlay: true },
+
+        // Cach 2 play nhac tu url
+        {uri: item.URI},
+        // {uri: selectedMusic.title},
+        {uri: item.author},
+
+        { shouldPlay: true }
+      );
+      setIsPlaying(true);
+      return setPlaybackStatus(status);
+    }
+
+    // It will pause our audio
+    if (playbackStatus.isPlaying) {
+      const status = await playbackObject.pauseAsync();
+      setIsPlaying(false);
+      return setPlaybackStatus(status);
+      
+    }
+
+    // It will resume our audio
+    if (!playbackStatus.isPlaying) {
+      const status = await playbackObject.playAsync();
+      
+      setIsPlaying(true);
+
+      return setPlaybackStatus(status);
+    }
+  };
+    // function handleSongPressed(){
+        
+    // }
     return (
-        <TouchableOpacity onPress={handleSongPressed}>
+        <TouchableOpacity onPress={handleAudioPlayPause}>
             <View style={styles.imageContainer}>
                 <Image
                     style={styles.image}
